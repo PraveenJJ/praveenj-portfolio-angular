871a2390d9471afddf82828d9b6f8742458449ab
PraveenJJ <praveen_jj123@yahoo.com>
2025-01-25 11:56:55 +0530

    Initial project structure

---
+++ src/app/sections/what-i-do/what-i-do.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-what-i-do',
+  standalone: true,
+  imports: [],
+  templateUrl: './what-i-do.component.html',
+  styleUrl: './what-i-do.component.css'
+})
+export class WhatIDoComponent {
+
+}
